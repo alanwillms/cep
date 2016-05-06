ae00336a566c53e5b6f9438aa97cd7fed022c849
Alan Willms <dyulax@gmail.com>
2016-05-06 00:30:38 -0300

    Implement file cache

--- src/cep.cr
+++ src/cep.cr
@@ -1,27 +1,52 @@
 require "http/server"
-require "./cep/*"
+require "file"
+require "tempfile"
+require "option_parser"
+# require "./cep/*"
 
 # Written into a single file for learning purposes (easier to read together)
 module Cep
   class Application
-    private getter :request, :response
+    private getter :request, :response, :tempdir
 
-    def initialize(@request : HTTP::Request, @response : HTTP::Server::Response)
+    def initialize(@request : HTTP::Request, @response : HTTP::Server::Response, @tempdir : String)
     end
 
     def run
-      cep = Cep::Cep.new(request.path.to_s[1..-1])
       if cep.valid?
-        response.content_type = "text/json; charset=UTF-8"
-        response.print Cep::ViaCep.get(cep)
+        print_json
       else
-        response.content_type = "text/plain; charset=UTF-8"
-        if cep.value == ""
-          response.print "Usage: /89805640"
-        else
-          response.print "CEP inválido: #{cep.value}"
-        end
+        print_instructions
       end
+    end
+
+    private def print_json
+      if cache.exists?
+        data = cache.get
+      else
+        data = Cep::ViaCep.new(cep).get_json
+        cache.set data
+      end
+
+      response.content_type = "text/json; charset=UTF-8"
+      response.print data
+    end
+
+    private def print_instructions
+      response.content_type = "text/plain; charset=UTF-8"
+      if cep.value == ""
+        response.print "Usage: /89805640"
+      else
+        response.print "CEP inválido: #{cep.value}"
+      end
+    end
+
+    private def cep
+      @cep ||= Cep::Cep.new(request.path.to_s[1..-1])
+    end
+
+    private def cache
+      @cache ||= Cache.new(cep, tempdir)
     end
   end
 
@@ -36,17 +61,60 @@ module Cep
     end
   end
 
+  class Cache
+    def initialize(@cep : Cep, @tmpdir : String)
+    end
+
+    def exists?
+      File.exists? file_name
+    end
+
+    def set(content)
+      File.write file_name, content
+      nil
+    end
+
+    def get
+      File.read file_name
+    end
+
+    private def file_name
+      ds = File::SEPARATOR_STRING
+      "#{@tmpdir}#{ds}#{@cep.value}.json"
+    end
+  end
+
   class ViaCep
-    def self.get(cep : Cep) : String
-      response = HTTP::Client.get "http://viacep.com.br/ws/#{cep.value}/json/"
+    def initialize(@cep : Cep)
+    end
+
+    def get_json : String
+      response = HTTP::Client.get "http://viacep.com.br/ws/#{@cep.value}/json/"
       response.body
     end
   end
 end
 
-server = HTTP::Server.new(3000) do |context|
-  Cep::Application.new(context.request, context.response).run
+port = 3000
+tempdir = Tempfile.dirname
+start = false
+
+parser = OptionParser.parse! do |parser|
+  parser.banner = "Usage: cep [arguments]"
+  parser.on("start", "Start the server") { start = true }
+  parser.on("-p NUMBER", "--port=NUMBER", "Server port") { |number| port = number.to_i32 }
+  parser.on("-t PATH", "--tempdir=PATH", "Temporary directory for cache") { |path| tempdir = path.to_s }
+  parser.on("-h", "--help", "Show this help") { puts parser }
 end
 
-puts "Listening on http://0.0.0.0:3000"
-server.listen
+if start
+  puts "Listening on http://0.0.0.0:#{port}"
+
+  server = HTTP::Server.new(port) do |context|
+    Cep::Application.new(context.request, context.response, tempdir).run
+  end
+
+  server.listen
+else
+  puts parser
+end
